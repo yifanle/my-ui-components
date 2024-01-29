61f95b7e3e0c712cddeb01d56de8ddad87773430
yifanle <2878725498@qq.com>
2024-01-29 23:13:32 +0800

    创建Icon组件

---
+++ src/components/Icon/icon.stories.js
@@ -0,0 +1,11 @@
+import Icon from ".";
+import React from 'react'
+
+import 'story.css'
+
+export default {
+    title: "Icon",
+    component: Icon,
+}
+
+export const Default = () => <Icon>默认</Icon>;
