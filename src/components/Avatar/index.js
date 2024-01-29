585104b2ae74f2efbf95f11b84a233867899f5b4
yifanle <2878725498@qq.com>
2024-01-29 22:30:26 +0800

    完成头像组件的编写

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import StyledAvatar, { AvatarClip, AvatarImage, StatusIcon } from './style'
+
+function Avatar({src, size = "48px", status, statusIconSize = "8px", ...rest}) {
+  return (
+    <StyledAvatar {...rest}>
+        { status && <StatusIcon status={status} size={statusIconSize}></StatusIcon>}
+        <AvatarClip size={size}>
+            <AvatarImage src={ src }/>
+        </AvatarClip>
+    </StyledAvatar>
+  )
+}
+
+Avatar.propTypes = {
+  src: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  status: PropTypes.oneOf(["online", "offline"]),
+  statusIconSize: PropTypes.string,
+}
+
+export default Avatar
+
