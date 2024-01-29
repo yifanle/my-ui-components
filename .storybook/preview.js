c12bb38a8d5a6a9dccb5b67a4a0337248a4442d4
yifanle <2878725498@qq.com>
2024-01-29 17:42:26 +0800

    聊天项目组件库

---
+++ .storybook/preview.js
@@ -0,0 +1,29 @@
+/** @type { import('@storybook/react').Preview } */
+
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from "../src/theme";
+
+export const decorators = [(Story) => (
+  <ThemeProvider theme={theme}><Story/></ThemeProvider>
+)]
+
+export const parameters = {
+  options: {
+    showRoots: true,
+  }
+}
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
