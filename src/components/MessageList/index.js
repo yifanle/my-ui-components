f6debb065e78b979aa3d3532e9521664f09c3f83
yifanle <2878725498@qq.com>
2024-02-03 19:36:01 +0800

    完成消息列表组件

---
+++ src/components/MessageList/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import StyledMessageList, { ChatList } from './style'
+import Filter from 'components/Filter'
+import Select from 'components/Select'
+import Option from 'components/Option'
+import Input from 'components/Input'
+import MessageCard from 'components/MessageCard'
+import face1 from 'assets/images/face-female-1.jpg'
+import Add16Filled from '@ricons/fluent/Add16Filled';
+import Button from 'components/Button'
+import Icon from 'components/Icon'
+
+function MessageList({ children, ...rest }) {
+  return (
+    <StyledMessageList {...rest}>
+        <Input.Search />
+        <ChatFilter/>
+        <ChatList>
+          {[1,2,3,4,5,6].map((_, index) => (
+            <MessageCard
+              key={index}
+              active={index===3}
+              replied={index%3 === 0}
+              avatarSrc={face1}
+              name="李敏镐"
+              avatarStatus="online"
+              statusText="在线"
+              time="3小时之前"
+              message="即使拍到最高的山上，一次也只能脚踏实地地"
+              unreadCound={2}
+            />
+          ))}
+        </ChatList>
+    </StyledMessageList>
+  )
+}
+
+function ChatFilter() {
+  return (
+    <Filter style={{ padding: "20px 0"}}>
+      <Filter.Filters label="列表排序">
+          <Select>
+              <Option>最新消息优先</Option>
+              <Option>在线好友优先</Option>
+          </Select>
+      </Filter.Filters>
+
+      <Filter.Action label="创建会话">
+          <Button><Icon icon={Add16Filled} width={12} height={12}/></Button>
+      </Filter.Action>
+    </Filter>
+  )
+}
+
+MessageList.propTypes = {
+  children: PropTypes.any
+}
+
+export default MessageList
+
---
+++ src/components/MessageList/messageList.stories.js
@@ -0,0 +1,11 @@
+import MessageList from ".";
+import React from 'react'
+
+import 'story.css'
+
+export default {
+    title: "页面组件/MessageList",
+    component: MessageList,
+}
+
+export const Default = () => <MessageList/>;
